0cdf07cc01c1e9e4518958c40c9c8bb3a68f4a4b
Peter-J-K <167132123+Peter-J-K@users.noreply.github.com>
2024-04-16 16:33:48 -0500

    Update streamlit_app.py
    Add Search_On to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -9,17 +10,15 @@ st.write(
     """Choose the fruits you want in your custom Smoothie
     """
 )
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 name_on_order = ''
 name_on_order = st.text_input('Name on Smoothie:', '')
 st.write('The name on your Smoothie will be: ' + name_on_order)
 
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list  = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
