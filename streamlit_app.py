cad751e1e9c55fe57ece788bb523678732cfba30
Peter-J-K <167132123+Peter-J-K@users.noreply.github.com>
2024-04-16 15:03:31 -0500

    Update streamlit_app.py
    Add dataframe for fruityvice info

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 name_on_order = ''
 name_on_order = st.text_input('Name on Smoothie:', '')
+st.write('The name on your Smoothie will be: ' + name_on_order)
+
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
@@ -45,4 +47,6 @@ if ingredients_list:
 # New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
